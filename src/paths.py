4f4c23179336c5901666de9cc314388af47a4632
Jose Caloca <jose.caloca3@gmail.com>
2024-09-03 13:34:35 +0200

    remove additional data folder

--- src/paths.py
+++ src/paths.py
@@ -1,16 +1,25 @@
 from pathlib import Path
 import os
 
-# Use the current working directory as a fallback when __file__ is not defined
-PARENT_DIR = Path(os.getcwd()).resolve()
+PARENT_DIR = Path(__file__).parent.resolve().parent
 DATA_DIR = PARENT_DIR / 'data'
-RAW_DATA_DIR = DATA_DIR / 'raw'
-TRANSFORMED_DATA_DIR = DATA_DIR / 'transformed'
-DATA_CACHE_DIR = DATA_DIR / 'cache'
+RAW_DATA_DIR = PARENT_DIR / 'data' / 'raw'
+TRANSFORMED_DATA_DIR = PARENT_DIR / 'data' / 'transformed'
+DATA_CACHE_DIR = PARENT_DIR / 'data' / 'cache'
 
 MODELS_DIR = PARENT_DIR / 'models'
 
-# Create directories if they don't exist
-for directory in [DATA_DIR, RAW_DATA_DIR, TRANSFORMED_DATA_DIR, DATA_CACHE_DIR, MODELS_DIR]:
-    directory.mkdir(parents=True, exist_ok=True)
-    
+if not Path(DATA_DIR).exists():
+    os.mkdir(DATA_DIR)
+
+if not Path(RAW_DATA_DIR).exists():
+    os.mkdir(RAW_DATA_DIR)
+
+if not Path(TRANSFORMED_DATA_DIR).exists():
+    os.mkdir(TRANSFORMED_DATA_DIR)
+
+if not Path(MODELS_DIR).exists():
+    os.mkdir(MODELS_DIR)
+
+if not Path(DATA_CACHE_DIR).exists():
+    os.mkdir(DATA_CACHE_DIR)
